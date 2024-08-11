711be245aaa46b63d0df252e6b4d0131e82330d8
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-10 22:45:08 -0600

    chore: Update server configuration and dependencies
    This commit updates the server configuration by importing and using the `body-parser` middleware for urlencoded requests. It also sets the limit to 16kb and enables extended parsing. Additionally, the server listen is added on the specified port.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
-import { urlencoded } from 'body-parser';
+import { urlencoded } from 'express';
 import express from 'express';
+import { Server } from 'socket.io';
+import 'dotenv/config'
 
 const app = express();
+const io = new Server();
+
 app.use(urlencoded({
     limit: "16kb",
     extended: true
 }))
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json({
+    limit: "16kb",
+    extended: true
+}))
 
-app.listen(PORT, () => {
-    console.log("Server is listening to port:", PORT);
+io.on('connection', socket => {
+    
 })
+
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const SOCKET_PORT = process.env.SOCKET_PORT || 3001;
+
+app.listen(HTTP_PORT, () => {
+    console.log("Http server is listening to port:", HTTP_PORT);
+})
+
+io.listen(SOCKET_PORT);
