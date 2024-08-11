dc554c94079995d216ee8b4a9e2ad8dca62d4fd3
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-10 23:16:08 -0600

    feat: Add Home component and update routing in App component
    - Add Home component to the pages directory
    - Update routing in App component to use react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className="text-2xl">App</div>
+    <Outlet/>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Home } from './pages/index.js'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App/>}>
+    <Route path="" element={<Home/>}/>
+  </Route>
+))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>      
   </StrictMode>,
 )
