abd09668b4fc11293c09a9e0d3b5b72653d7cb87
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-11 11:02:21 -0600

    refactor: Update Lobby component to include remote email ID
    The Lobby component has been updated to include the display of the remote email ID in the UI. This allows users to see the email ID of the peer they are connected to in the lobby. This enhancement improves the user experience and provides better context during video streaming sessions.

--- client/src/pages/Lobby/Lobby.jsx
+++ client/src/pages/Lobby/Lobby.jsx
@@ -6,18 +6,21 @@ export default function Lobby() {
   const { socket } = useSocket();
   const {peer, createOffer, createAnswer, setRemoteAnswer, sendStream, remoteStream} = usePeer();
   const[myStream, setMyStream] = useState(null);
+  const[remoteEmailId, setRemoteEmailId] = useState(null);
   
 
   const handleUserJoined = useCallback(async({email}) => {
         console.log(`User: ${email} joined the room!`);
         const offer = await createOffer();
         socket.emit('call-user', {email, offer});
+        setRemoteEmailId(email);
   }, [socket, peer])
 
   const handleIncomingCall = useCallback(async({from, offer}) => {
     console.log("Incoming call from ", offer);
     const answer = await createAnswer(offer);
     socket.emit('call-accepted', {email: from, answer })
+    setRemoteEmailId(from)
   }, [createAnswer, socket])
 
   const handleCallAccepted = useCallback(async({answer}) => {
@@ -46,9 +49,23 @@ export default function Lobby() {
     getUserMediaStream();
   }, [getUserMediaStream])
 
+  const handleNegotiation = useCallback(() => {
+    const localOffer = peer.localDescription;
+    socket.emit('call-user', {email: remoteEmailId, offer: localOffer});
+  }, [peer.localDescription, remoteEmailId, socket])
+
+
+  useEffect(() => {
+    peer.addEventListener('negotiationneeded', handleNegotiation)
+    return () => {
+        peer.removeEventListener('negotiationneeded', handleNegotiation)
+    }
+  })
+
   return (
     <>
-      <div>Lobby</div>
+      <h1>Lobby</h1>
+      <h4>You are connected to {remoteEmailId}</h4>
       <button onClick={(() => sendStream(myStream))}>Send my video</button>
       <ReactPlayer url={myStream} playing/>
       <ReactPlayer url={remoteStream} playing/>
